2ba485d308aabe3a415bc1273eddca8d73638b22
Kyuhee Jo <kyuhee0622@gmail.com>
2021-09-26 01:19:52 -0400

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import logo from './logo.svg';
 import './App.scss';
 import Project from './Project.js';
-import Homepage from './Homepage.js';
 import Bubble from './Bubble.js';
 import ReactDOM from "react-dom"
-import ResumePage from './resume';
 import {
   BrowserRouter as Router,
   Switch,
@@ -14,18 +12,98 @@ import {
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Homepage />
-        </Route>
-        <Route path="/resume">
-          <ResumePage/>
-        </Route>
-      </Switch>
-    </Router>
 
-   
+    <div className="App">
+      <div id="particle-container">
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+        <div class="particle"></div>
+      </div>
+      <div class="navbar" style={{"position":"fixed", "top": "0px", "left":"0px"}}>
+        <div class="navlist-wrapper">
+          <ul>
+            <li class="navbar-item">Home</li>
+            <a href="https://drive.google.com/file/d/1rlUDqrZ-80NVaJFr_dq-81zT69rAWx56/view?usp=sharing"><li class="navbar-item">Resume</li></a>
+            <a href="#project"><li class="navbar-item">Projects</li></a>
+          </ul>
+        </div>
+      </div>
+      <header className="App-header">
+       <h1 className = 'title'> KYUHEE JO </h1>
+       <h3 className = 'email'> Hacker, Researcher, Developer </h3>
+       <div className="logo-container">
+        <svg className="Social-logo" viewBox = "0 0 496 512" color = "#31d33">
+          <a className="Social-logo" href="https://github.com/kyuheejo" target="_blank">
+            <path fill="currentColor" d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 
+            3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 
+            2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 
+            8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 
+            0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 
+            72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 
+            2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 
+            61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 
+            75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 
+            1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 
+            1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 
+            1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 
+            0-6.2-1.4-2.3-4-3.3-5.6-2z">
+            </path>
+          </a>
+        </svg>
+        <svg className="Social-logo" viewBox = "0 0 496 512" color = "#31d33">
+          <a className="Social-logo" href="https://linkedin.com/in/kyuheejo" target="_blank">
+          <path fill="currentColor" d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"></path>
+          </a>
+        </svg>
+        <svg className="Social-logo" viewBox = "0 0 496 512" color = "#31d33">
+        <a className="Social-logo" href="https://kjo3.medium.com/" target="_blank">
+        <path fill="currentColor" d="M71.5 142.3c.6-5.9-1.7-11.8-6.1-15.8L20.3 72.1V64h140.2l108.4 237.7L364.2 64h133.7v8.1l-38.6 37c-3.3 2.5-5 6.7-4.3 10.8v272c-.7 4.1 1 8.3 4.3 10.8l37.7 37v8.1H307.3v-8.1l39.1-37.9c3.8-3.8 3.8-5 3.8-10.8V171.2L241.5 447.1h-14.7L100.4 171.2v184.9c-1.1 7.8 1.5 15.6 7 21.2l50.8 61.6v8.1h-144v-8L65 377.3c5.4-5.6 7.9-13.5 6.5-21.2V142.3z"></path>
+        </a>
+        </svg>
+      </div>
+      </header>
+      <div id="project" style={{paddingBottom:"70px", backgroundColor:"#F9F1F0"}}>
+      <Project></Project>
+      </div>
+  
+      <center style={{backgroundColor:"#F9F1F0"}}>
+        <hr style={{opactiy:80,width:"30%", backgroundColor:"#F9F1F0", margin:"0px", paddingLeft:"10px", paddingRight:"10px"}}>
+        </hr></center>
+      <div className="footer" style={{backgroundColor:"#F9F1F0"}}>
+        <h5 className="comments" >Built from scratch using React, HTML/CSS and JavaScript </h5>
+        <h5 className="comments" > © Kyuhee Jo kjo3@jhu.edu</h5>
+      </div>
+  
+    </div>
+    
+
 
   );
 }
--- src/Project.js
+++ src/Project.js
@@ -9,6 +9,10 @@ import { black } from 'chalk';
 import { REGEX_BACKSLASH } from 'picomatch/lib/constants';
 import { useState } from 'react';
 import sparetime from './images/sparetime.png'
+import or from './images/orscheduling.png'
+import dino from './images/dino.png'
+import spheroid from './images/yarn.png'
+import prince from './images/prince.png'
 
 
 function Project() {
@@ -21,7 +25,11 @@ function Project() {
   const [startTime, setTime] = useState(Date.now());
 
   return (
+    
     <div className="project-container">
+      <div style={{width:"100%", paddingTop: "50px", paddingBottom: "20px"}}>
+      <h1 className="project-category" data-aos="fade-right" data-aos-duration="700" data-aos-delay="100">Mobile Application</h1>
+      </div>
       <div className="project-card">
       <a href="https://devpost.com/software/guidedog-smart-sunglasses-for-the-blind">
         <div style={{backgroundImage: `url(${guidedog}`, backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat:"no-repeat"}} className="project-item">
@@ -45,8 +53,71 @@ function Project() {
       
       </div>
       <div className="project-card">
+        <a href="https://docs.google.com/presentation/d/1nbb1V8Z8FblYuvm0ALzkDCCE3suLEa6g6tfSCUaRSgg/edit?usp=sharing">
+          <div style={{backgroundImage: `url(${sparetime}`, backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat:"no-repeat"}} className="project-item">
+          
+            <div style={{backgroundColor: "rgba(0, 0, 0, 0.6)", margin: "0"}} className="project-description">
+            <p class="p my-auto mx-auto w-75" style={{color: "white"}}> Android Application </p>
+            </div>
+            
+          </div>
+        </a>
+        <div className="bubble-container">
+          <Bubble delay="0" name="iOS application"></Bubble>
+          <Bubble delay="25" name="Python"></Bubble>
+          <Bubble delay="50" name="Objective-C"></Bubble>
+          <Bubble delay="150" name="Firebase"></Bubble>
+          <Bubble delay="200" name="Beautiful Soup"></Bubble>
+          <Bubble delay="250" name="Selenium"></Bubble>
+        </div>
+      
+      </div>
+      <div style={{width:"100%", paddingTop: "100px", paddingBottom: "20px"}}>
+      <h1 className="project-category" data-aos="fade-right" data-aos-duration="700" data-aos-delay="500">Web Application</h1>
+      </div>
+      <div className="project-card">
       <a href="https://docs.google.com/presentation/d/1nbb1V8Z8FblYuvm0ALzkDCCE3suLEa6g6tfSCUaRSgg/edit?usp=sharing">
-        <div style={{backgroundImage: `url(${sparetime}`, backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat:"no-repeat"}} className="project-item">
+        <div style={{backgroundImage: `url(${or}`, backgroundSize: "contain", backgroundPosition: "center", backgroundRepeat:"no-repeat"}} className="project-item">
+        
+          <div style={{backgroundColor: "rgba(0, 0, 0, 0.6)", margin: "0"}} className="project-description">
+          <p class="p my-auto mx-auto w-75" style={{color: "white"}}> Android Application </p>
+          </div>
+          
+        </div>
+        </a>
+        <div className="bubble-container">
+          <Bubble delay="0" name="Java"></Bubble>
+          <Bubble delay="50" name="HTML/CSS"></Bubble>
+          <Bubble delay="150" name="Spring Boot"></Bubble>
+          <Bubble delay="200" name="Tabu Search"></Bubble>
+        </div>
+      
+      </div>
+      <div className="project-card">
+      <a href="https://docs.google.com/presentation/d/1nbb1V8Z8FblYuvm0ALzkDCCE3suLEa6g6tfSCUaRSgg/edit?usp=sharing">
+        <div style={{backgroundImage: `url(${spheroid}`, backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat:"no-repeat"}} className="project-item">
+        
+          <div style={{backgroundColor: "rgba(0, 0, 0, 0.6)", margin: "0"}} className="project-description">
+          <p class="p my-auto mx-auto w-75" style={{color: "white"}}> Android Application </p>
+          </div>
+          
+        </div>
+        </a>
+        <div className="bubble-container">
+          <Bubble delay="0" name="Python"></Bubble>
+          <Bubble delay="50" name="Tensorflow"></Bubble>
+          <Bubble delay="150" name="Keras"></Bubble>
+          <Bubble delay="200" name="JavaScript"></Bubble>
+          <Bubble delay="250" name="HTML/CSS"></Bubble>
+        </div>
+      
+      </div>
+      <div style={{width:"100%", paddingTop: "100px", paddingBottom: "20px"}}>
+      <h1 className="project-category" data-aos="fade-right" data-aos-duration="700" data-aos-delay="500">ML/AI Research</h1>
+      </div>
+      <div className="project-card">
+      <a href="https://docs.google.com/presentation/d/1nbb1V8Z8FblYuvm0ALzkDCCE3suLEa6g6tfSCUaRSgg/edit?usp=sharing">
+        <div style={{backgroundImage: `url(${dino}`, backgroundSize: "contain", backgroundPosition: "center", backgroundRepeat:"no-repeat"}} className="project-item">
         
           <div style={{backgroundColor: "rgba(0, 0, 0, 0.6)", margin: "0"}} className="project-description">
           <p class="p my-auto mx-auto w-75" style={{color: "white"}}> Android Application </p>
@@ -67,13 +138,28 @@ function Project() {
       
       </div>
       <div className="project-card">
-        <div className="project-item"></div>
-        <Bubble></Bubble>
+      <a href="https://docs.google.com/presentation/d/1nbb1V8Z8FblYuvm0ALzkDCCE3suLEa6g6tfSCUaRSgg/edit?usp=sharing">
+        <div style={{backgroundImage: `url(${prince}`, backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat:"no-repeat"}} className="project-item">
+        
+          <div style={{backgroundColor: "rgba(0, 0, 0, 0.6)", margin: "0"}} className="project-description">
+          <p class="p my-auto mx-auto w-75" style={{color: "white"}}> Android Application </p>
+          </div>
+          
+        </div>
+        </a>
+        <div className="bubble-container">
+          <Bubble delay="0" name="Android"></Bubble>
+          <Bubble delay="50" name="Python"></Bubble>
+          <Bubble delay="150" name="Java"></Bubble>
+          <Bubble delay="200" name="Tensorflow"></Bubble>
+          <Bubble delay="250" name="Flask"></Bubble>
+          <Bubble delay="300" name="ML Kit"></Bubble>
+          <Bubble delay="300" name="Google Cloud API"></Bubble>
+          <Bubble delay="350" name="Azure Vision API"></Bubble>
+        </div>
+      
       </div>
-      <div className="project-card">
-        <div className="project-item"></div>
-        <Bubble></Bubble>
-      </div>
+      
 
 
 
